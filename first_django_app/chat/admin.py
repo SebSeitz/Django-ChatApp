2746d32c339b4234c991bb53e68539ffe6cde5af
SebSeitz <sebastian_seitz13@web.de>
2022-07-26 22:33:51 +0200

    added message model

--- first_django_app/chat/admin.py
+++ first_django_app/chat/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text', 'created_at', 'author')
+    list_display = ('created_at', 'author', 'text') # show this info in a list; make order as you wish
+    search_fields =  ('text',)
+
 # Register your models here.
+
+admin.site.register(Message, MessageAdmin)
