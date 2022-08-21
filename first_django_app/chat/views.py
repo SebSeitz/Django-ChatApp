15f9d22a9c7285572fdc7ea679bfdfe443d14802
SebSeitz <sebastian_seitz13@web.de>
2022-08-21 21:55:29 +0200

    removing node_modules

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -6,6 +6,8 @@ from django.contrib import messages
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.core import serializers # means that object is being transformed into the correcte form
 
 # Create your views here.
 @login_required(login_url='/login/')
@@ -13,7 +15,9 @@ def index(request):
    if request.method == 'POST':
       print("received data " + request.POST['textmessage'])
       myChat = Chat.objects.get(id=1)
-      Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+      new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+      serialized_obj = serializers.serialize('json', [new_message])
+      return JsonResponse(serialized_obj[1:-1], safe=False)
    chatMessages = Message.objects.filter(chat__id=1) # Indenting is important here!
    return render(request, 'chat/index.html', {'messages': chatMessages})
 
